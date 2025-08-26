f273078414d74c6dd3f6a04e4eb9af42dbfb06d0
DavidKlein <krepp-magenta.0z@icloud.com>
2025-08-26 12:20:25 +0200

    änderung

---
+++ Schule/src/blatt01/StringCodes.java
@@ -0,0 +1,9 @@
+package blatt01;
+
+public class StringCodes {
+    public static void main(String[] args) {
+        int gewinn_september = 221000;
+        int gewinn_oktober = 1009520;
+        System.out.println("Monat\t\t\tGewinn\nSeptember 2017\t" + gewinn_september + "€\nOktober 2017\t" + gewinn_oktober + "€");
+    }
+}
