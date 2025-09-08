c6a7bbc11a9a633880d29dce4c6c5804988c1fbf
DavidKlein <krepp-magenta.0z@icloud.com>
2025-09-08 09:30:58 +0200

    -

---
+++ Schule/src/blatt04/etf.java
@@ -0,0 +1,23 @@
+package blatt04;
+
+public class etf {
+    public static void main(String[] args) {
+        double sparrate = 50;
+        double gesamtcash;
+        double gewinn = 0.06;
+        double anteil;
+        int monat;
+        int monatjahr;
+        while (monat <= 36) {
+            monat += 1;
+            monatjahr += 1;
+            gesamtcash += sparrate;
+            anteil = gesamtcash / 3500;
+            if (monatjahr == 12) {
+                gesamtcash += gesamtcash * anteil;
+                monatjahr = 0;
+            }
+        }
+        System.out.println("Gesamtcash: " + gesamtcash + "\nMonat: " + monat + "\n");
+    }
+}
