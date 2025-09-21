c3590d3a042d7b5cc2c1e52fa09a00ca6187dacf
Dominavid <krepp-magenta.0z@icloud.com>
2025-09-21 10:30:16 +0200

    blatt 4 fertig

--- Schule/src/blatt05/Übungdiagramme.java
+++ Schule/src/blatt05/Übungdiagramme.java
@@ -7,15 +7,23 @@ public class Übungdiagramme {
         Scanner input = new Scanner(System.in);
         System.out.println("Was ist die Anzahl Eurer Noten, Sir?");
         int anzahl = input.nextInt();
+        int a = anzahl;
         int noten = 0;
         int note = 0;
+        double schnitt;
 
-        while (anzahl > 0) {
-            while (note < 1 && note > 6) {
-                System.out.println("Welche Note haben Euer Exzellenz erreicht?");
-                note = input.nextInt();
+        while (a > 0) {
+            System.out.println("Welche Note haben Euer Exzellenz erreicht?");
+            note = input.nextInt();
+            if (note > 6 || note < 1) {
+                System.out.println("Die Note muss zwischen 1 und 6 liegen, ich fürchte Euer Exzellenz haben einen Tippfehler gemacht.");
+                note = 0;
+                a++;
             }
-
+            noten += note;
+            a--;
         }
+        schnitt = noten / anzahl;
+        System.out.printf("Euer Exzellenz haben einen Notendurchschnitt von %.2f erreicht. \nDieser Wert wurde zur Veranschaulichung auf zwei Nachkommastellen gerundet.\n", schnitt);
     }
 }
