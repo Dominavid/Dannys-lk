b0bcaf96ccf905a9ec198f0e52b1ac756265b1f6
Dominavid <krepp-magenta.0z@icloud.com>
2025-09-22 09:38:29 +0200

    timo ist glücklich weil es jetzt arrays gibt (:

---
+++ Schule/src/blatt06/TimosLieblingsDatentyp.java
@@ -0,0 +1,25 @@
+package blatt06;
+
+import java.util.Scanner;
+
+public class TimosLieblingsDatentyp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Wie viele Buben sind in dem Kurs?");
+        int[] noten = new int[Math.abs(input.nextInt())];
+
+        for (int i = 0; i < noten.length; i++) {
+            System.out.println("Welche Note hat der " + (i + 1) + ". Bube erreicht?");
+            noten[i] = input.nextInt();
+            if (noten[i] < 1 || noten[i] > 6) {
+                System.out.println("Böses Foul! Die Note gibt's nicht.");
+                System.exit(0);
+            }
+            if (noten[i] > 3) {
+                System.out.println("Was ein Dödel!\n");
+            } else {
+                System.out.println("So ein fähiger Mann!\n");
+            }
+        }
+    }
+}
