d351592d4ed45c10b31872c0d6ba5b754af6ab37
Dominavid <krepp-magenta.0z@icloud.com>
2026-03-02 22:19:02 +0100

    ich weiß eh nd wo ich die scheiße lesen kann...

--- Schule/src/blatt17/GameOfLife.java
+++ Schule/src/blatt17/GameOfLife.java
@@ -21,33 +21,38 @@ public class GameOfLife {
     }
 
 
+    public static initOszillierend
+
+
     public static void weiter() {
         char[][] spielfeldkopie = blatt14.MultiArrays.copy2DCharArray(spielfeld);
 
         for (int i = 0; i < spielfeld.length; i++) {
             for (int j = 0; j < spielfeld[i].length; j++) {
-                int nachbarn1 = blatt14.Simulationen.zaehlenAcht(spielfeldkopie, i, j, '1', false);
                 if (spielfeldkopie[i][j] == '0') {
-                    if (blatt14.Simulationen.zaehlenAcht(spielfeldkopie, i, j, '0', false) == 3) {
+                    if (blatt14.Simulationen.zaehlenAcht(spielfeldkopie, i, j, '1', false) == 3) {
                         spielfeld[i][j] = '1';
                     }
-                } else if (nachbarn1 < 2 || nachbarn1 > 3) {
-                    spielfeld[i][j] = '0';
+                } else {
+                    int nachbarn = blatt14.Simulationen.zaehlenAcht(spielfeldkopie, i, j, '1', false);
+                    if (nachbarn < 2 || nachbarn > 3) {
+                        spielfeld[i][j] = '0';
+                    }
                 }
             }
         }
 
-        if (spielfeldkopie == spielfeld) {
+        if (blatt14.MultiArrays.istIdentisch(spielfeldkopie, spielfeld)) {
             lüppt = false;
         }
     }
 
     public static void main(String[] args) {
         SchischVisualizer danny = new SchischVisualizer();
-        initRandom(0.05);
+        initRandom(0.1);
         danny.step(spielfeld);
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             weiter();
             danny.step(spielfeld);
             if (!lüppt) {
