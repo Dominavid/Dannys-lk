9aa75e02a42250a63a4a304ce4b14d7045af9c0c
DavidKlein <krepp-magenta.0z@icloud.com>
2026-03-09 09:31:15 +0100

    wo kann ich das lesen?

---
+++ Schule/src/blatt19/QuickSort.java
@@ -0,0 +1,29 @@
+package blatt19;
+
+public class QuickSort {
+    public static int[] trennenLinks(int[] arr, int pivotpos) {
+        if (pivotpos <= 0) {
+            System.out.println("Schere: Ungültige Position");
+            return null;
+        } //todo: machen
+        int[] zarr = new int[pivotpos];
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+        return zarr;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        if (System.getProperty("user.name") == "TimoDorn") {
+            System.out.println("Hallo, Dino!");
+        } else {
+            boolean ausgestorben = true;
+        }
+
+
+    }
+}
